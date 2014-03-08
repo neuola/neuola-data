734cb26c1f64e601ccbc73cc581ee77c5e6cdced
yfwz100 <yfwz100@yeah.net>
2014-03-09 00:31:43 +0800

    Support for mocha test framework.

---
+++ test/test.js
@@ -0,0 +1,12 @@
+/*globals describe, it */
+/*jslint eqeq: true, indent: 2, node: true, plusplus: true, regexp: true, unparam: true, vars: true, nomen: true */
+'use strict';
+
+var assert = require('assert');
+
+describe('Models', function () {
+  it('Initialization', function () {
+    require('../');
+  });
+  
+});
