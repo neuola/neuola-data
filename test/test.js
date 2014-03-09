026c79a419a0bbe8776021e7c55c56444038ac81
yfwz100 <yfwz100@yeah.net>
2014-03-09 10:33:44 +0800

    Add test for post model.

--- test/test.js
+++ test/test.js
@@ -4,15 +4,37 @@
 
 var assert = require('assert');
 var tungus = require('tungus');
+var os = require('os');
 
 describe('Models', function () {
   var model = require('../');
-  model('tingodb:///local');
+  model('tingodb://' + os.tmpdir() + '/neuola-data-tingodb');
 
-  it('#save', function () {
-    var post = new model.Post();
-    post.save(function (err, doc) {
-      assert.ok(doc);
+  describe('Post', function () {
+    
+    it('#save', function () {
+      var post = new model.Post({
+        authorId: 'tester',
+        title: 'Hello',
+        content: 'world!'
+      });
+      post.save(function (err, doc) {
+        if (err) {
+          throw new Error(err);
+        }
+        assert.ok(doc);
+      });
+    });
+    
+    it('.findOne', function () {
+      var q = model.Post.findOne({authorId: 'tester'});
+      q.exec(function (err, post) {
+        if (err) {
+          throw new Error();
+        }
+        assert.ok(post);
+        assert.equal('tester', post.authorId);
+      });
     });
   });
 
