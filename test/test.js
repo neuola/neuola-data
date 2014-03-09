f040fa334fafbc542ea9e505eb1d1df47ec94304
yfwz100 <yfwz100@yeah.net>
2014-03-09 10:22:29 +0800

    Initial test.

--- test/test.js
+++ test/test.js
@@ -1,12 +1,19 @@
-/*globals describe, it */
+/*globals describe, it, beforeEach */
 /*jslint eqeq: true, indent: 2, node: true, plusplus: true, regexp: true, unparam: true, vars: true, nomen: true */
 'use strict';
 
 var assert = require('assert');
+var tungus = require('tungus');
 
 describe('Models', function () {
-  it('Initialization', function () {
-    require('../');
+  var model = require('../');
+  model('tingodb:///local');
+
+  it('#save', function () {
+    var post = new model.Post();
+    post.save(function (err, doc) {
+      assert.ok(doc);
+    });
   });
-  
+
 });
