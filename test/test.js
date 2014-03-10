2a502a41e75c1ee4345dddc2cfd9c40ab6a62492
yfwz100 <yfwz100@yeah.net>
2014-03-11 00:07:59 +0800

    Clean directory of test DB before test.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,20 @@
-/*globals describe, it, beforeEach */
+/*globals describe, it, before */
 /*jslint eqeq: true, indent: 2, node: true, plusplus: true, regexp: true, unparam: true, vars: true, nomen: true */
 'use strict';
 
 var tungus = require('tungus');
 var os = require('os');
 
+var dbPath = os.tmpdir() + '/neuola-data-tingodb';
+
+// Clean the directory before test.
+before(function () {
+  var fs = require('fs');
+  var files = fs.readdirSync(dbPath);
+  files.forEach(function (file) {
+    fs.unlinkSync(dbPath + '/' + file);
+  });
+});
+
 var model = require('../');
-model('tingodb://' + os.tmpdir() + '/neuola-data-tingodb');
+model('tingodb://' + dbPath);
